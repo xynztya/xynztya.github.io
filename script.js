abc695f2ccf322e5348e9b14740c6e6a454b4f12
Xynz <23411985+xynz@users.noreply.github.com>
2020-02-12 17:13:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
   var options = {
     strings: [".security", ".linux", ".seo", ".php", ".server", ".hack", "etc.."],
-    typeSpeed: 50,
+    typeSpeed: 40,
     starDelay: 200,
     backDelay: 600,
     loop: true,
